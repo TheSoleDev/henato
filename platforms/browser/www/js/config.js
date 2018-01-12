b602c840143b6a7ee518f2c0521753f9ad3e1a97
juldio <garciajuliusb@yahoo.com>
2018-01-12 22:11:30 +0800

    connect to API

--- platforms/browser/www/js/config.js
+++ platforms/browser/www/js/config.js
@@ -1,2 +1,2 @@
-var src_url = 'http://henato-v2.local/storage/';
-var api_root = 'http://henato-v2.local/';
+var src_url = 'http://henato.eglobalmd.com/storage/';
+var api_root = 'http://henato.eglobalmd.com/';
